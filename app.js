67186189368c9953b0f4ffa984084fa63dd3c36c
AlexanderVak <mamorunokitsune@gmail.com>
2021-07-30 10:32:35 +0300

    add logRequest function

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+function logRequest ({method, url}, res, next) {
+    console.log(`[${new Date().toISOString()}] ${method} ${url}`);
+    next()
+}
+
 app.use(express.json())
+app.use(logRequest)
+
 
 const tasks = [{ name: 'First task' }, { name: 'Second task' }]
 app.get('/tasks', (req, res) => { res.json(tasks) })
@@ -12,7 +19,6 @@ app.post('/tasks', (req, res) => {
     tasks.push(task)
     res.json(task)
 })
-
 
 app.listen(port, () => {
     console.log(`Server started at localhost:${port}`);
