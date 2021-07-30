6c2f812d066f9cb593aba3e0219c8524a82e2142
AlexanderVak <mamorunokitsune@gmail.com>
2021-07-30 10:53:42 +0300

    add patch request

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-function logRequest ({method, url}, res, next) {
+function logRequest({ method, url }, res, next) {
     console.log(`[${new Date().toISOString()}] ${method} ${url}`);
     next()
 }
@@ -10,14 +10,24 @@ function logRequest ({method, url}, res, next) {
 app.use(express.json())
 app.use(logRequest)
 
-
-const tasks = [{ name: 'First task' }, { name: 'Second task' }]
+const tasks = [{ id: 1, title: 'First task' }, { id: 2, title: 'Second task' }]
 app.get('/tasks', (req, res) => { res.json(tasks) })
 
 app.post('/tasks', (req, res) => {
     const task = req.body
     tasks.push(task)
     res.json(task)
+})
+
+app.patch('/tasks/:id', (req, res) => {
+    const taskId = parseInt(req.params.id)
+    const task = tasks.find(t => t.id === taskId)
+    if (task) {
+        Object.assign(task, req.body)
+        res.json(task)
+    } else {
+        res.status(404).json({ error: 'Task not found' })
+    }
 })
 
 app.listen(port, () => {
