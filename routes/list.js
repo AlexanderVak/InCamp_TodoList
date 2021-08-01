77c8abc6347af981dee3731870dd0aedcae4b0ad
Olexandr Vakulinskiy <mamorunokitsune@gmail.com>
2021-08-01 21:25:31 +0300

    add list routes

---
+++ routes/list.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import controller from '../controllers/listController.js'
+import RestfulRoutes from './restfulRoutes.js'
+const router = express.Router()
+
+RestfulRoutes(router, controller)
+    .crud()
+
+export default router
