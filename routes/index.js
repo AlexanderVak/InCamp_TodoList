f4169d565a5aa70f39aed5f803ff8510cb2041ab
Olexandr Vakulinskiy <mamorunokitsune@gmail.com>
2021-08-01 21:25:46 +0300

    add lists to router

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 const router = express.Router()
 import tasks from './task.js'
+import lists from './list.js'
 
 router
     .use('/tasks', tasks)
+    .use('/lists', lists)
 
 export default router
