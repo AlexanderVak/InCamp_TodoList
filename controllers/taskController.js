20b2590b74b0cc1b4240bac63cafaedf9e258b2a
Olexandr Vakulinskiy <mamorunokitsune@gmail.com>
2021-08-01 14:47:34 +0300

    add task controller

---
+++ controllers/taskController.js
@@ -0,0 +1,20 @@
+import Task from '../models/task.js'
+
+class TaskController {
+    find(){
+        return Task.find()
+    }
+    create(task){
+        return Task.create(task)
+    }
+    findById(id){
+        return Task.findById(id)
+    }    
+    removeById(id){
+        return Task.findByIdAndRemove()
+    }
+    updateById(id, task){
+        return Task.findByIdAndUpdate(id, task)
+    }
+}
+export default new TaskController()
