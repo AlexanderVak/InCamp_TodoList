951bb379929c14f85e4435cb653382b6d15c165d
Olexandr Vakulinskiy <mamorunokitsune@gmail.com>
2021-08-01 16:26:34 +0300

    add test to rewrite function

--- tests/task.spec.js
+++ tests/task.spec.js
@@ -1,5 +1,5 @@
 import Task from '../models/task.js'
-describe('Test Task class', () => {
+describe('Task class', () => {
     const tasks = [
         { id: 1, title: 'First task', done: false },
         { id: 2, title: 'Second task', done: false }
@@ -7,19 +7,31 @@ describe('Test Task class', () => {
     const newTask = { id: 3, title: 'New task', done: false }
     const updatedTask = { id: 2, title: 'Second task', done: true }
     it('should return all tasks', () => {
-        expect(Task.find()).toStrictEqual(tasks)        
+        expect(Task.find()).toStrictEqual(tasks)
     });
     it('should return specific task', () => {
-        expect(Task.findById(2)).toStrictEqual(tasks[1])        
+        expect(Task.findById(2)).toStrictEqual(tasks[1])
     });
     it('should create new task', () => {
-        expect(Task.create({title: 'New task'})).toStrictEqual(newTask)        
+        expect(Task.create({ title: 'New task' })).toStrictEqual(newTask)
     });
     it('should remove specific task', () => {
-        expect(Task.findByIdAndRemove(3)).toStrictEqual(tasks)        
+        expect(Task.findByIdAndRemove(3)).toStrictEqual(tasks)
     });
     it('should update specific task', () => {
-        expect(Task.findByIdAndUpdate(2, {done: true})).toStrictEqual(updatedTask)        
+        expect(Task.findByIdAndUpdate(2, { done: true })).toStrictEqual(updatedTask)
+    });
+    it('should rewrite specific task', () => {
+        expect(Task.findByIdAndRewrite(2, {
+            title: "Rewriten title",
+            done: "true",
+            rewrite: true
+        })).toStrictEqual({
+            id: 2,
+            title: "Rewriten title",
+            done: "true",
+            rewrite: true
+        })
     });
 
 });
