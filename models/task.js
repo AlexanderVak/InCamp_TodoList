ac174f51a1a9b46e3975c1030e772a11e3384d36
AlexanderVak <mamorunokitsune@gmail.com>
2021-08-06 18:05:31 +0300

    rename tasQuery to tasks  & start working at taskExist func

--- models/task.js
+++ models/task.js
@@ -2,33 +2,39 @@ import pool from '../db/index.js'
 
 class Task {
 
+    async taskExists(id) {
+        let result = await pool.query('SELECT * FROM tasks WHERE EXISTS(SELECT id FROM tasks WHERE id=$1)', [id])
+        console.log(result);
+    }
+
     async find() {
-        let tasksQuery = await pool.query('SELECT * FROM tasks ORDER BY id')
-        return tasksQuery.rows
+        let tasks = await pool.query('SELECT * FROM tasks ORDER BY id')
+        return tasks.rows
     }
 
     async create(task) {
-        pool.query('INSERT INTO tasks (title, due_date) VALUES ($1, $2)', [task.title, new Date(task.dueDate)])
-        
-        let tasksQuery = await pool.query('SELECT * FROM tasks ORDER BY id DESC LIMIT 1')
-        console.log(tasksQuery.rows);
-        return tasksQuery.rows
+        await pool.query('INSERT INTO tasks (title, due_date) VALUES ($1, $2)', [task.title, new Date(task.dueDate)])
+
+        let tasks = await pool.query('SELECT * FROM tasks ORDER BY id DESC LIMIT 1')
+        console.log(tasks.rows);
+        return tasks.rows
     }
 
     async findById(id) {
-        let tasksQuery = await pool.query('SELECT * FROM tasks WHERE id=$1', [id])
-        return tasksQuery.rows
+        let tasks = await pool.query('SELECT * FROM tasks WHERE id=$1', [id])
+        this.taskExists(id)
+        return tasks.rows
     }
 
     async findByIdAndRemove(id) {
-        let tasksQuery = await pool.query('DELETE FROM tasks WHERE id=$1', [id])
-        return tasksQuery.rows
+        let tasks = await pool.query('DELETE FROM tasks WHERE id=$1', [id])
+        return tasks.rows
     }
     async findByIdAndReplace(id, task) {
         await pool.query('UPDATE tasks SET title=$1, done=$2, due_date=$3 WHERE id=$4', [task.title, task.done, task.dueDate, id])
         return this.findById(id)
     }
-    
+
     async findByIdAndUpdate(id, task) {
         await pool.query('UPDATE tasks SET done=$1 WHERE id=$2', [task.done, id])
         return this.findById(id)
