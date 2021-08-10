81deded7b9d17231694dd9414fb105fdc60feab2
AlexanderVak <mamorunokitsune@gmail.com>
2021-08-10 16:03:09 +0300

    lists on knex

--- models/list.js
+++ models/list.js
@@ -1,44 +1,63 @@
-import pool from "../db/index.js"
+import db from "../db/index.js"
 
 class List {
     async listExists(list) {
-        if (list.rows.length > 0) {
-            return list.rows
+        if (list.length > 0) {
+            return list
         } else {
             return false
         }
     }
 
     async find() {
-        let lists = await pool.query('SELECT * FROM lists ORDER BY id')
-        return lists.rows
+        //pool.query('SELECT * FROM lists ORDER BY id')
+        let lists = await db.select().table('lists').orderBy('id')
+        return lists
     }
 
     async create(list) {
-        await pool.query('INSERT INTO lists (title) VALUES ($1)', [list.title])
-
-        let lists = await pool.query('SELECT * FROM lists ORDER BY id DESC LIMIT 1')
-        return lists.rows
+        //pool.query('INSERT INTO lists (title) VALUES ($1)', [list.title])
+        let lists = await db('lists')
+            .returning(['id', 'title'])
+            .insert({ title: list.title })
+        return lists
     }
 
     async findById(id) {
-        let list = await pool.query('SELECT * FROM lists WHERE id=$1', [id])
+        //pool.query('SELECT * FROM lists WHERE id=$1', [id])
+        let list = await db('lists').where('id', id)
         return await this.listExists(list)
     }
 
     async findByIdAndRemove(id) {
-        let list = await pool.query('DELETE FROM lists WHERE id=$1', [id])
+        //pool.query('DELETE FROM lists WHERE id=$1', [id])
+        let list = await db('lists')
+            .where('id', id)
+            .del()
+
         return await this.listExists(list)
     }
 
-    async findByIdAndReplace(id, task) {
-        await pool.query('UPDATE lists SET title=$1, tasks=$2 WHERE id=$4', [task.title, task.tasks, id])
-        return this.findById(id)
+    async findByIdAndReplace(id, list) {
+        //pool.query('UPDATE lists SET title=$1, tasks=$2 WHERE id=$4', [task.title, task.tasks, id])
+        let lists = await db('lists')
+            .returning(['id', 'title'])
+            .where('id', id)
+            .update({
+                title: list.title
+            })
+        return lists
     }
 
-    async findByIdAndUpdate(id, task) {
-        await pool.query('UPDATE tasks SET title=$1 WHERE id=$2', [task.done, id])
-        return this.findById(id)
+    async findByIdAndUpdate(id, list) {
+        //pool.query('UPDATE tasks SET title=$1 WHERE id=$2', [task.done, id])
+        let lists = await db('lists')
+            .returning(['id', 'title'])
+            .where('id', id)
+            .update({
+                title: list.title
+            })
+        return lists
     }
 }
 export default List
