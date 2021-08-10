5ac9ab3cd9a95e91e7f37026c2f765559b7b700e
AlexanderVak <mamorunokitsune@gmail.com>
2021-08-10 11:28:33 +0300

    remove extra query from dashboard

--- models/dashboard.js
+++ models/dashboard.js
@@ -1,11 +1,6 @@
 import pool from "../db/index.js"
 export default class Dashboard {
-    dashboardQuery = `SELECT COUNT(tasks.id) as planned_tasks
-    FROM tasks
-    WHERE due_date between $1 and tasks.due_date
-    AND done=false`;
-
-    dashboardUnfinishedQuery = `SELECT COUNT(tasks.id) as unfinished_tasks, lists.id as list_id, lists.title as list_title
+    dashboardUnfinishedQuery = `SELECT COUNT(*) as unfinished_tasks, lists.id as list_id, lists.title as list_title
     FROM lists
     LEFT JOIN tasks ON lists.id = tasks.list_id
     WHERE tasks.due_date between $1 and tasks.due_date AND tasks.done=false 
