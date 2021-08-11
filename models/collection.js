af24647d8852a992c5b268e2f8bf40d2b494b5ed
AlexanderVak <mamorunokitsune@gmail.com>
2021-08-11 11:19:34 +0300

    collections on knex

--- models/collection.js
+++ models/collection.js
@@ -1,12 +1,15 @@
-import pool from "../db/index.js"
+import db from "../db/index.js"
 export default class Collection {
     todayQuery = `SELECT tasks.id, tasks.title as task_title, lists.title as list_title
                   FROM tasks
                   RIGHT JOIN lists ON tasks.list_id = lists.id
                   WHERE tasks.due_date=CURRENT_DATE`
     async tasksForToday(){
-        let tasks = await pool.query(this.todayQuery)
-        return tasks.rows
+        let tasks = await db('tasks')
+            .rightJoin('lists', 'tasks.list_id', 'lists.id')
+            .select('tasks.id', 'tasks.title as task_title', 'lists.title as list_title')
+            .where('tasks.due_date', new Date())
+        return tasks
     }
     
 }
