bb0014196a5b10e96f85037613e1b33456ef7188
AlexanderVak <mamorunokitsune@gmail.com>
2021-08-11 16:30:06 +0300

    define list table

--- models/list.js
+++ models/list.js
@@ -1,63 +1,19 @@
-import db from "../db/index.js"
+import Sequelize from 'sequelize'
+import sequelize from '../config/config.js'
+import Task from './task.js'
 
-class List {
-    async listExists(list) {
-        if (list.length > 0) {
-            return list
-        } else {
-            return false
-        }
+const List = sequelize.define('list', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    title: {
+        type: Sequelize.STRING,
     }
+}, { tableName: 'lists' })
 
-    async find() {
-        //pool.query('SELECT * FROM lists ORDER BY id')
-        let lists = await db.select().table('lists').orderBy('id')
-        return lists
-    }
+List.hasMany(Task)
 
-    async create(list) {
-        //pool.query('INSERT INTO lists (title) VALUES ($1)', [list.title])
-        let lists = await db('lists')
-            .returning(['id', 'title'])
-            .insert({ title: list.title })
-        return lists
-    }
-
-    async findById(id) {
-        //pool.query('SELECT * FROM lists WHERE id=$1', [id])
-        let list = await db('lists').where('id', id)
-        return await this.listExists(list)
-    }
-
-    async findByIdAndRemove(id) {
-        //pool.query('DELETE FROM lists WHERE id=$1', [id])
-        let list = await db('lists')
-            .where('id', id)
-            .del()
-
-        return await this.listExists(list)
-    }
-
-    async findByIdAndReplace(id, list) {
-        //pool.query('UPDATE lists SET title=$1, tasks=$2 WHERE id=$4', [task.title, task.tasks, id])
-        let lists = await db('lists')
-            .returning(['id', 'title'])
-            .where('id', id)
-            .update({
-                title: list.title
-            })
-        return lists
-    }
-
-    async findByIdAndUpdate(id, list) {
-        //pool.query('UPDATE tasks SET title=$1 WHERE id=$2', [task.done, id])
-        let lists = await db('lists')
-            .returning(['id', 'title'])
-            .where('id', id)
-            .update({
-                title: list.title
-            })
-        return lists
-    }
-}
 export default List
