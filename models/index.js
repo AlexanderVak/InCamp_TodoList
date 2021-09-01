a0f6c55ff9baed2a05b7b236872d6278078fdce6
AlexanderVak <mamorunokitsune@gmail.com>
2021-09-01 18:01:45 +0300

    remove extra foreign key

--- models/index.js
+++ models/index.js
@@ -2,6 +2,6 @@ import List from "./list.js";
 import Task from "./task.js";
 
 List.hasMany(Task)
-Task.belongsToMany(List, { through: List, foreignKey: "list_id"})
+Task.belongsTo(List, { through: List})
 
 export {List, Task}
