76c07b11e70994637fe8ade70cc65a9a216e3d81
AlexanderVak <mamorunokitsune@gmail.com>
2021-09-01 12:48:15 +0300

    add sequelize to TaskController

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,23 +1,54 @@
 import Task from '../models/task.js'
-let tasks = new Task()
 class TaskController {
     find(listId, queryParams) {
-        return tasks.find(listId, queryParams)
+        if (queryParams) {
+            return Task.findAll({
+                where: { list_id: listId },
+                groupBy: 'id'
+            })
+        } else {
+            return Task.findAll({
+                where: {
+                    list_id: listId,
+                    done: false
+                },
+                groupBy: 'id'
+            })
+        }
     }
+
     create(task, listId) {
-        return tasks.create(task, listId)
+        return Task.create({ ...task, list_id: listId })
     }
     findById(id, listId) {
-        return tasks.findById(id, listId)
+        return Task.findOne({
+            where:{ id: id, list_id: listId }
+        })
     }
     removeById(id, listId) {
-        return tasks.findByIdAndRemove(id, listId)
+        return Task.destroy({
+            where:{ id: id, list_id: listId }
+        })
     }
     replace(id, listId, task) {
-        return tasks.findByIdAndReplace(id, listId, task)
+        return Task.update({
+            id: id,
+            title: task.title,
+            done: task.done,
+            due_date: task.dueDate,
+            list_id: list_id
+        },
+        {
+            where: { id: id, list_id: listId },
+            returning: true
+        })
     }
     updateById(id, listId, task) {
-        return tasks.findByIdAndUpdate(id, listId, task)
+        return Task.update({ done: task.done },
+            {
+                where: { id: id, list_id: listId },
+                returning: true
+            })
     }
 }
 export default new TaskController()
